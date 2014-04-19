88d545f4b5da41de9075f50d889836480cde8b02
jawerty <jawerty210@gmail.com>
2014-04-19 13:34:37 -0400

    better design

--- js/chef.js
+++ js/chef.js
@@ -26,5 +26,33 @@ var Chef = {
 }
 
 $(document).ready(function(){
+	createRecipe = $("#createRecipe")
+	recipes = $("#recipes")
+	plusSign = true;
+
+	$("#dropDownForm").on("click", function(){
+		if (plusSign == true) {
+			$("#dropDownForm img").attr("src", "images/minus_sign.png")
+			plusSign = false
+		} else {
+			$("#dropDownForm img").attr("src", "images/plus_sign.png")
+			plusSign = true
+		}
+		
+		createRecipe.slideToggle(250)
+
+		console.log("this")
+	})
+	$("#dropDownList").on("click", function(){
+		if (plusSign == true) {
+			$("#dropDownList img").attr("src", "images/minus_sign.png")
+			plusSign = false
+		} else {
+			$("#dropDownList img").attr("src", "images/plus_sign.png")
+			plusSign = true
+		}
+		recipes.slideToggle(250)
+		console.log("that")
+	})
 	Chef.init()
 });
